b7d968095c4663e5f68552d6c1342b64030bae61
Cherif Kooistra <cherif@Cherifs-MacBook-Air.local>
2025-11-30 02:52:06 +0100

    Fix: Cast children to any for React 19 compat

--- components/smooth-scroll.tsx
+++ components/smooth-scroll.tsx
@@ -8,7 +8,7 @@ export function SmoothScroll({ children }: { children: React.ReactNode }) {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
-            {children}
+            {children as any}
         </ReactLenis>
     )
 }
